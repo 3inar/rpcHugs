d1c8afdf40087ed8071091a55912a824655ed2d2
Einar H <edwynbot@gmail.com>
2013-04-17 17:00:06 +0200

    more extensive tests

--- example.py
+++ example.py
@@ -1,11 +1,16 @@
 from main import RPC, Dummy
 # demo of how to rpc:
+
+
 class _testRpc(RPC):
     def __init__(self, port=0):
         RPC.__init__(self, port)
 
     def add(self, a, b):
         return a + b
+
+    def echo(self, val):
+        return val
 
 if __name__ == "__main__":
     try:
@@ -18,13 +23,23 @@ if __name__ == "__main__":
         remote_address = callee_rpc.server_info()
         callee = Dummy(caller_rpc, remote_address)
 
-        num_tests = 10000
+        num_tests = 100
         print 'adding two numbers ' + str(num_tests) + ' times via RPC'
         for i in range(num_tests):
-            value = callee.add(i, i**2)
+            i_sq = i**2
+            value = callee.add(i, i_sq)
+            assert value == (i + i_sq)
             sys.stdout.write('\r' + str(int(i*100.0/num_tests)) + ' %')
         sys.stdout.write('\rDONE       \n')
         sys.stdout.flush()
+
+        # the following can safely be out-commented if you have no PIL
+        from PIL import Image
+        image = Image.open('CLOUDS.jpg')
+        string = image.tostring("jpeg", "RGB").encode("base64")
+
+        callee.echo(string)
+
     except:
         sys.stdout.write('\n')
         sys.stdout.flush()
